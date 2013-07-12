7817d1d62f0a2c6d6a5c616b581588899f86ec45
Travis Chase <travis@travischase.me>
2013-07-12 14:12:54 -0400

    initial creation

---
+++ lib/font/awesome/sass.rb
@@ -0,0 +1,9 @@
+require "font/awesome/sass/version"
+
+module Font
+  module Awesome
+    module Sass
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/font/awesome/sass/version.rb
@@ -0,0 +1,7 @@
+module Font
+  module Awesome
+    module Sass
+      VERSION = "0.0.1"
+    end
+  end
+end
