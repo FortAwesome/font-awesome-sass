9a540df378e3f03dfd3cd3d707a1ab86a1b88708
Scott McCormack <flintinatux@gmail.com>
2013-07-22 23:50:22 -0400

    Provide assets to Sprockets if Rails not available

---
+++ lib/font/awesome/sass/sprockets.rb
@@ -0,0 +1,4 @@
+require 'sprockets'
+
+Sprockets.append_path File.expand_path('../../../../../vendor/assets/fonts', __FILE__)
+Sprockets.append_path File.expand_path('../../../../../vendor/assets/stylesheets', __FILE__)
