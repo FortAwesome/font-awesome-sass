fc32fc3e5985be36eca44f53615c6c2fb976c74f
Noah Kantrowitz <noah@coderanger.net>
2013-12-05 16:36:08 -0800

    Helpers for non-Rails usage.

--- lib/font/awesome/sass.rb
+++ lib/font/awesome/sass.rb
@@ -1,8 +1,24 @@
 module Font
   module Awesome
     module Sass
-      require 'font/awesome/sass/engine'
+      require 'font/awesome/sass/engine' if defined?(Rails)
       require 'font/awesome/sass/version'
+
+      def self.gem_path
+        File.expand_path('../../../..', __FILE__)
+      end
+
+      def self.assets_path
+        File.join(gem_path, 'vendor', 'assets')
+      end
+
+      def self.fonts_path
+        File.join(assets_path, 'fonts')
+      end
+
+      def self.stylesheets_path
+        File.join(assets_path, 'stylesheets')
+      end
     end
   end
 end
