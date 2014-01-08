38bda8115e744407174e5fdc9cf5d00285a986e1
Cyril Suzat <cyril.suzat@me.com>
2014-01-08 14:40:53 +0100

    Remove unwanted space at the end of the tag class

--- lib/font/awesome/sass/view_helpers.rb
+++ lib/font/awesome/sass/view_helpers.rb
@@ -3,7 +3,7 @@ module Font
     module Sass
       module ViewHelpers
         def icon(icon, text=nil, options = {})
-          options[:class] = ["fa fa-#{icon}", options[:class].to_s].join(' ')
+          options[:class] = ["fa", "fa-#{icon}"].concat(Array(options[:class])).join(' ')
           html = content_tag(:i, nil, options)
           html << " #{text}" if text
           html
