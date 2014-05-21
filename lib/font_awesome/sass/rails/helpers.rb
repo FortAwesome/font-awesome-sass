8a02385790067a2d4659f8a2b27ccea4f2374a0d
Ernest Walzel <ewalzel@gmail.com>
2014-05-22 00:26:52 +0100

    The 'text' argument for 'icon' helper method made optional HTML options can now be specified without passing an empty argument

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -2,7 +2,8 @@ module FontAwesome
   module Sass
     module Rails
       module ViewHelpers
-        def icon(icon, text="", html_options={})
+        def icon(icon, text=nil, html_options={})
+          text, html_options = nil, text if text.is_a?(Hash)
           content_class = "fa fa-#{icon}"
           content_class << " #{html_options[:class]}" if html_options.key?(:class)
           html_options[:class] = content_class
