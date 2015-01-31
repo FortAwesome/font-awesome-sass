4a6f3d6ad31870c4a47ad157334175833bd7c923
Jarred Trost <jtrost@gmail.com>
2015-01-31 13:29:46 -0600

    Added stacked_icon helper

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -11,6 +11,13 @@ module FontAwesome
           html << " #{text}" unless text.blank?
           html.html_safe
         end
+
+        def stacked_icon(icon1, icon2, html_options={})
+          content_class = "fa-stack"
+          content_class << " #{html_options[:class]}" if html_options.key?(:class)
+          html_options[:class] = content_class
+          content_tag(:span, icon1+icon2, html_options).html_safe
+        end
       end
     end
   end
