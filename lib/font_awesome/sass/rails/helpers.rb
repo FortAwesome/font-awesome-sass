4874b7e0a729d08dd2132ab2adfa2c5948a72592
kobaltz <dave@k-innovations.net>
2015-03-13 11:55:03 -0400

    undefined method `key?' for nil:NilClass
    Fixes undefined method `key?' for nil:NilClass error for simple icon usage
    
    i.e. <%= icon('home') %>

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -7,7 +7,7 @@ module FontAwesome
           html_options = text if text.is_a?(Hash)
           
           content_class = "fa fa-#{icon}"
-          content_class << " #{html_options[:class]}" if html_options.key?(:class)
+          content_class << " #{html_options[:class]}" if html_options && html_options.key?(:class)
           html_options[:class] = content_class
 
           html = content_tag(:i, nil, html_options)
