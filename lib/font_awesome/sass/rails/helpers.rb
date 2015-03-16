61bf0db8b833eeb569c38bd8251f649f821459a1
Arjun Venkataswamy <avenkat2@gmail.com>
2015-03-16 15:43:50 -0500

    adds fix for icon helper when no optional arguments are present

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -5,7 +5,8 @@ module FontAwesome
         def icon(icon, *args)
           text, html_options = args
           html_options = text if text.is_a?(Hash)
-          
+          html_options = {} if html_options.blank?
+
           content_class = "fa fa-#{icon}"
           content_class << " #{html_options[:class]}" if html_options.key?(:class)
           html_options[:class] = content_class
