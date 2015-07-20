e5225e6dbb904d3b5d51c871c14ddd9da92dca76
Brian Jenkins <bonkydog@bonkydog.com>
2015-07-20 14:46:59 -0700

    Fix Rails asset pre-compilation
    Icons were showing up as boxes in staging and 
    production because the Rails engine initializer
    wasn't being added to the assets group.
    
    Most other projects seem to add the engine to the
    'all' group so that's what I did here.

--- lib/font_awesome/sass/rails/engine.rb
+++ lib/font_awesome/sass/rails/engine.rb
@@ -2,7 +2,7 @@ module FontAwesome
   module Sass
     module Rails
       class Engine < ::Rails::Engine
-        initializer 'font-awesome-sass.assets.precompile' do |app|
+        initializer 'font-awesome-sass.assets.precompile', group: :all do |app|
           %w(stylesheets fonts).each do |sub|
             app.config.assets.paths << root.join('assets', sub).to_s
           end
