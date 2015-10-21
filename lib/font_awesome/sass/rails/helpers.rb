b35feb51b7667d2c64721590c2d62d0ad86f1198
Andrey Kulakevich <andrey@watzan.com>
2015-10-21 13:14:32 -0300

    Add option to provide an Array of classes for icon.
    Removed trailing white space.

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -6,7 +6,13 @@ module FontAwesome
           text, html_options = nil, text if text.is_a?(Hash)
 
           content_class = "fa fa-#{icon}"
-          content_class << " #{html_options[:class]}" if html_options.key?(:class)
+          if html_options.key?(:class)
+            if html_options[:class].is_a? Array
+              content_class << " #{html_options[:class].compact.join(' ')}"
+            else
+              content_class << " #{html_options[:class]}"
+            end
+          end
           html_options[:class] = content_class
 
           html = content_tag(:i, nil, html_options)
