c03e695801336e5f5ee6fcedc15ddf78fa3ddc6d
Andrey Kulakevich <andrey@watzan.com>
2015-11-23 10:44:31 -0400

    Allow multiple fa classes.

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -5,13 +5,14 @@ module FontAwesome
         def icon(icon, text = nil, html_options = {})
           text, html_options = nil, text if text.is_a?(Hash)
 
-          content_class = "fa fa-#{icon}"
+          icons = icon.is_a?(Array) ? icon : icon.to_s.split(" ")
+          icons.map! { |i| "fa-#{i}" }
+          content_class = "fa #{icons.join(' ')}"
           if html_options.key?(:class)
             if html_options[:class].is_a? Array
-              content_class << " #{html_options[:class].compact.join(' ')}"
-            else
-              content_class << " #{html_options[:class]}"
+              html_options[:class] = html_options[:class].compact.join(' ')
             end
+            content_class << " #{html_options[:class]}"
           end
           html_options[:class] = content_class
 
