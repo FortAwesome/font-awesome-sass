c6bea84cd68bf34820baa0485b9120ed491383ba
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-08-24 12:51:13 +0900

    Refactoring manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -3,15 +3,12 @@ description 'FontAwesome for Sass'
 # Stylesheet importing font-awesome
 stylesheet 'styles.sass'
 
-# Copy JS and fonts
+# Copy fonts
 manifest = Pathname.new(File.dirname(__FILE__))
 assets   = File.expand_path('../../assets', manifest)
-{:javascript => 'javascripts',
- :font       => 'fonts'
-}.each do |method, dir|
-  root = Pathname.new(assets).join(dir)
-  Dir.glob root.join('**', '*.*') do |path|
-    path = Pathname.new(path)
-    send method, path.relative_path_from(manifest).to_s, :to => path.relative_path_from(root).to_s
-  end
+
+fonts = Pathname.new(assets).join('fonts')
+Dir.glob fonts.join('**', '*.*') do |path|
+  path = Pathname.new(path)
+  font path.relative_path_from(manifest).to_s, :to => path.relative_path_from(fonts).to_s
 end
