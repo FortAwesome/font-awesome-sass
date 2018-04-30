7314bffb68e2cacfdd416fdb4f17986ba9f05404
Jeremiah Megel <jeremiah@entretechno.com>
2018-04-30 16:41:33 -0500

    Pass HTML class attribute as array instead of string

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -5,8 +5,8 @@ module FontAwesome
         def icon(style, name, text = nil, html_options = {})
           text, html_options = nil, text if text.is_a?(Hash)
 
-          content_class = "#{style} fa-#{name}"
-          content_class << " #{html_options[:class]}" if html_options.key?(:class)
+          content_class = [style, "fa-#{name}"]
+          content_class << html_options[:class] if html_options.key?(:class)
           html_options[:class] = content_class
 
           html = content_tag(:i, nil, html_options)
